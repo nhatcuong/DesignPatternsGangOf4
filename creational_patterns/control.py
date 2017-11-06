14bce41e4b00ef7849b2e66009da0388ff159c70
Nguyen Nhat Cuong <nguyen.nhat.cuong@gmail.com>
2017-11-06 01:21:05 +0100

    creational_patterns --> control

---
+++ creational_patterns/control.py
@@ -0,0 +1,85 @@
+from enum import Enum
+
+
+class Direction(Enum):
+    NORTH = 0
+    SOUTH = 1
+    EAST = 2
+    WEST = 3
+
+
+class MapSite:
+    def enter(self):
+        raise NotImplementedError
+
+
+class Room(MapSite):
+    def __init__(self, room_no):
+        self._room_no = room_no
+        self._sides = [None] * 4
+
+    def get_side(self, direction):
+        return self._sides[direction.value]
+
+    def set_side(self, direction, map_site):
+        self._sides[direction.value] = map_site
+
+    def get_no(self):
+        return self._room_no
+
+
+class Wall(MapSite):
+    pass
+
+
+class Door(MapSite):
+    def __init__(self, room1, room2):
+        self._room1 = room1
+        self._room2 = room2
+        self._is_open = False
+
+    def other_side_room(self, room):
+        if self._room1 == room:
+            return self._room2
+        if self._room2 == room:
+            return self._room1
+        raise ValueError('door {} is not by room {}'.format(self, room))
+
+
+class Maze:
+    def __init__(self):
+        self.room_dict = dict()
+
+    def add_room(self, room):
+        self.room_dict[room.get_no()] = room
+
+    def get_room(self, no):
+        try:
+            return self.room_dict[no]
+        except ValueError:
+            raise ValueError('Maze {} does not have room no {}'.format(self, no))
+
+
+def create_maze():  #create a maze with 2 rooms and a door in between
+    maze = Maze()
+    room1 = Room(1)
+    room2 = Room(2)
+    door = Door(room1, room2)
+    maze.add_room(room1)
+    maze.add_room(room2)
+
+    room1.set_side(Direction.NORTH, Wall())
+    room1.set_side(Direction.SOUTH, Wall())
+    room1.set_side(Direction.WEST, Wall())
+    room1.set_side(Direction.EAST, door)
+
+    room1.set_side(Direction.NORTH, Wall())
+    room1.set_side(Direction.SOUTH, Wall())
+    room1.set_side(Direction.WEST, door)
+    room1.set_side(Direction.EAST, Wall())
+
+    print('maze created')
+    return maze
+
+
+create_maze()
---
+++ creational_patterns/readme.md
@@ -0,0 +1,9 @@
+## About creational patterns
+
+control.py shows the base implementation without any pattern.
+Then there will be 5 patterns to be implemented:
+- Abstract Factory
+- Builder
+- Factory Method
+- Prototype
+- Singleton
